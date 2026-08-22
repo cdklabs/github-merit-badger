b229eda253d9dbd2419ea902318b817c85a22796
cdklabs-automation <90142015+cdklabs-automation@users.noreply.github.com>
2026-08-22 00:06:28 +0000

    chore(deps): upgrade dependencies (#710)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]:
    https://github.com/cdklabs/github-merit-badger/actions/runs/32539054513
    
    ------
    
    *Automatically created by projen via the "upgrade" workflow*
    
    ---------
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- dist/index.js
+++ dist/index.js
@@ -6376,8 +6376,6 @@ exports.Deprecation = Deprecation;
 "use strict";
 
 
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-
 /*!
  * is-plain-object <https://github.com/jonschlinkert/is-plain-object>
  *
